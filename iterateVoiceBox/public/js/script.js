6242951b910b05a24812133e4e103eda3f680d31
Andrew Swift <andrew.swift@norwegian.no>
2013-09-03 08:12:46 +0200

    Provided a resource containing all supported words and created a simple player that is able to transmit which words to play from the client

--- iterateVoiceBox/public/js/script.js
+++ iterateVoiceBox/public/js/script.js
@@ -1,27 +1,46 @@
-(function ($) {
+(function ($, io) {
     // connect to socket.io
-    var socket = io.connect('http://' + window.location.host);
+    var socket = io.connect('http://' + window.location.host)
 
-    // send message on click
-    $('.btn-primary').on('click', function (e) {
-        e.preventDefault();
-        socket.emit('controller', { message: $(this).data('message') });
+    //creates the socket.io player
+    function createPlayer(opts) {
+        if(typeof opts !== 'object')
+            opts = {};
 
-        function playWord(vel) {
-            socket.emit('notedown', {message: vel, channel: 1});
-            socket.emit('noteup', {message: vel, channel: 1});
+        var delay = opts.delay || 1000
+        function playWord(word) {
+            var index = word;
+            socket.emit('notedown', {message: index, channel: 1});
+            socket.emit('noteup', {message: index, channel: 1});
         }
 
-        var i = 1
-        var timer = setTimeout(function() {
+        return {
+            say: function (list) {
+                var index = 0;
+                var timer = setTimeout(function () {
+                    //call the calling function after the delay
+                    timer = setTimeout(arguments.callee, delay);
+                    if (list.length > index) {
+                        playWord(list[index]);
+                        index++;
+                    }
+                    else {
+                        clearTimeout(timer);
+                    }
 
-            timer = setTimeout(arguments.callee, 400)
-
-            playWord(i);
-            if (i++==20) {
-                clearTimeout(timer)
+                }, 0)
             }
-        }, 0)
+        }
+    }
+
+    var player = createPlayer({delay:200});
+
+    // Send a hello message
+    $('.btn-primary').on('click', function (e) {
+        var words = [11, 12, -1, 13, -1, -1, 10, -1, 23];
+
+        player.say(words);
     });
 
-})(jQuery);
+
+})(jQuery, io);
--- iterateVoiceBox/server.js
+++ iterateVoiceBox/server.js
@@ -1,18 +1,24 @@
 var express = require('express')
     , http = require('http')
     , talkBox = require('./lib/iterate-voice-box.js')
-    , midi = require('./lib/iterate-midi.js');
+    , midi = require('./lib/iterate-midi.js')
+    , dictionary = require('./lib/dictionary.js');
 
 var app = express(),
     server = http.createServer(app),
     talkBox = talkBox.listen(server);
 
 app.use(express.static(__dirname + '/public'));
+app.get('/words', function(req, res) {
+    res.json(dictionary.words);
+});
+
 
 server.listen(process.env.PORT || 3000)
 
 
 midi.connect();
+
 talkBox.player(midi.in,midi.out);
 
 process.on("SIGTERM", function () {
