a8dafede5c914b83bdf1ecbd34472af07ac24eb8
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2013-09-04 19:44:34 +0200

    Add constructor to Note
    Because it looked like it wanted one... poor note.

--- WordMapper/src/main/java/no/iterate/javazone/Note.java
+++ WordMapper/src/main/java/no/iterate/javazone/Note.java
@@ -4,6 +4,11 @@ public class Note {
     private int note;
     private int velocity;
 
+    public Note(int note, int velocity) {
+        this.note = note;
+        this.velocity = velocity;
+    }
+
     public int getNote() {
         return note;
     }
