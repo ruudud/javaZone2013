13557d090bfb9ca061eabcbef428ec5cc8a8f4c7
Andrew Swift <andrew.swift@norwegian.no>
2013-09-04 19:07:16 +0200

    Created test and skeleton for Dictionary builder

---
+++ dictionaryBuilder/src/main/java/no/iterate/javazone/DictionaryBuilder.java
@@ -0,0 +1,13 @@
+package no.iterate.javazone;
+
+/**
+ * Hello world!
+ *
+ */
+public class DictionaryBuilder
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dictionaryBuilder/src/test/java/no/iterate/javazone/DictionaryBuilderTest.java
@@ -0,0 +1,38 @@
+package no.iterate.javazone;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DictionaryBuilderTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DictionaryBuilderTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DictionaryBuilderTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
