16cfe6bb083984c4ea8decba4058b3789d9cd001
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2013-09-04 19:36:52 +0200

    Refactor tests and add test to fetch sinlge note

--- WordMapper/src/test/java/no/iterate/javazone/WordMapperTest.java
+++ WordMapper/src/test/java/no/iterate/javazone/WordMapperTest.java
@@ -1,5 +1,6 @@
 package no.iterate.javazone;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
@@ -8,34 +9,48 @@ import static org.junit.Assert.assertEquals;
 
 public class WordMapperTest {
 
+    WordMapper wordMapper;
+    String wordListAsJson =
+            "{\n" +
+                    "   \"Application\" : {\n" +
+                    "       \"note\" : 123,\n" +
+                    "       \"velocity\" : 38,\n" +
+                    "       \"channel\" : 1\n" +
+                    "   },\n" +
+                    "   \"Java\" :  {\n" +
+                    "       \"note\" : 124,\n" +
+                    "       \"velocity\" : 25,\n" +
+                    "       \"channel\" : 1\n" +
+                    "   },\n" +
+                    "   \n" +
+                    "   \"Iterate\" : {\n" +
+                    "       \"note\" : 22,\n" +
+                    "       \"velocity\"  : 34,\n" +
+                    "       \"channel\" : 1\n" +
+                    "   }\n" +
+                    "}";
+
+    @Before
+    public void setUp() {
+        wordMapper = new WordMapper();
+        wordMapper.setWords(wordListAsJson);
+    }
+
     @Test
     public void mapJsonToWordObjects() {
-        String wordListAsJson =
-                "{\n" +
-                        "   \"Application\" : {\n" +
-                        "       \"note\" : 123,\n" +
-                        "       \"velocity\" : 38,\n" +
-                        "       \"channel\" : 1\n" +
-                        "   },\n" +
-                        "   \"Java\" :  {\n" +
-                        "       \"note\" : 124,\n" +
-                        "       \"velocity\" : 22,\n" +
-                        "       \"channel\" : 1\n" +
-                        "   },\n" +
-                        "   \n" +
-                        "   \"Iterate\" : {\n" +
-                        "       \"note\" : 22,\n" +
-                        "       \"velocity\"  : 34,\n" +
-                        "       \"channel\" : 1\n" +
-                        "   }\n" +
-                        "}";
-
-        WordMapper wordMapper = new WordMapper();
-        wordMapper.setWords(wordListAsJson);
 
         Map<String,Note> words = wordMapper.getWords();
+
         assertEquals(3, words.size());
         assertEquals(123, words.get("Application").getNote());
-        assertEquals(22, words.get("Java").getVelocity());
+        assertEquals(25, words.get("Java").getVelocity());
+    }
+
+    @Test
+    public void getNoteObjectForWord() {
+        Note iterateNote = wordMapper.getWords().get("Iterate");
+
+        assertEquals(22, iterateNote.getNote());
+        assertEquals(34, iterateNote.getVelocity());
     }
 }
