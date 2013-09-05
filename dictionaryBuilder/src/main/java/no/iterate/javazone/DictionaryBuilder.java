daf4d6492600dcd58546d2b51b74405e4e03073b
Andrew Swift <andrew.swift@norwegian.no>
2013-09-05 09:03:31 +0200

    Implementation complete

--- dictionaryBuilder/src/main/java/no/iterate/javazone/DictionaryBuilder.java
+++ dictionaryBuilder/src/main/java/no/iterate/javazone/DictionaryBuilder.java
@@ -1,13 +1,78 @@
 package no.iterate.javazone;
 
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
- * Hello world!
+ * Converts a list of words into json formatted
+ *
+ *  a,b,c -> - { word : {note: x, velocity: y, channel : 1 }}
+ *
+ *
+ * and converts it
  *
  */
 public class DictionaryBuilder
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+    public DictionaryBuilder() {
+    }
+
+    public static void main( String[] args ) {
+
+        try {
+            new DictionaryBuilder().readAndWrite();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readAndWrite() throws IOException {
+        InputStream inputStream = DictionaryBuilder.class.getResourceAsStream("/wordsToSample.txt");
+
+        try {
+            System.out.println(readStuff(inputStream));
+        }
+        finally {
+            inputStream.close();
+        }
+    }
+
+    private String readStuff(InputStream inputStream) throws IOException {
+        InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8"); // leave charset out for default
+        BufferedReader bufferedReader= new BufferedReader(reader);
+
+        return toJson(bufferedReader);
+    }
+
+    public String toJson(BufferedReader bufferedReader) throws IOException {
+        StringBuilder result = new StringBuilder("{");
+        int note = 0;
+        int velocity = 1;
+        Map<String, Map<String, Integer>> messages = new LinkedHashMap<>();
+        String word;
+        while ((word = bufferedReader.readLine()) != null) {
+
+            if (velocity > 127) {
+                note++;
+                velocity=1;
+                result.append("\n\t");
+            }
+
+            HashMap<String, Integer> notes = new LinkedHashMap<>();
+            notes.put("note", note);
+            notes.put("velocity", velocity);
+            notes.put("channel", 1);
+            messages.put(new String(word), notes);
+
+            velocity++;
+
+        }
+        return new Gson().toJson(messages);
     }
 }
--- dictionaryBuilder/src/test/java/no/iterate/javazone/DictionaryBuilderTest.java
+++ dictionaryBuilder/src/test/java/no/iterate/javazone/DictionaryBuilderTest.java
@@ -1,38 +1,25 @@
 package no.iterate.javazone;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Explain the responsibility of this class
  */
-public class DictionaryBuilderTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public DictionaryBuilderTest(String testName)
-    {
-        super( testName );
-    }
+public class DictionaryBuilderTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( DictionaryBuilderTest.class );
-    }
+    @Test
+    public void testToJson() throws Exception {
+        DictionaryBuilder dictionaryBuilder = new DictionaryBuilder();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        String test = "a\nb\nc\nd\ne\nf\ng\nh\n";
+
+        test = dictionaryBuilder.toJson(new BufferedReader(new StringReader(test)));
+
+        assertEquals("{\"a\":{\"note\":1,\"velocity\":0,\"channel\":1},\"b\":{\"note\":2,\"velocity\":0,\"channel\":1},\"c\":{\"note\":3,\"velocity\":0,\"channel\":1},\"d\":{\"note\":4,\"velocity\":0,\"channel\":1},\"e\":{\"note\":5,\"velocity\":0,\"channel\":1},\"f\":{\"note\":6,\"velocity\":0,\"channel\":1},\"g\":{\"note\":7,\"velocity\":0,\"channel\":1},\"h\":{\"note\":8,\"velocity\":0,\"channel\":1}}",test);
     }
 }
